ec9cc1950b92f476278787d68eb34fb95b15560f
Maboroshy <maboroshy@mail.ru>
2024-06-16 10:50:25 +0300

    Make OscQueryMaker allow to include data into queries

--- midiscripter/osc/osc_query_maker.py
+++ midiscripter/osc/osc_query_maker.py
@@ -6,7 +6,9 @@ from midiscripter.osc.osc_port import OscIn, OscOut
 
 
 class OscQueryMaker:
-    """The helper class to handle OSC request - response queries."""
+    """The helper class to handle OSC request - response queries.
+    Should be a permanent object rather than created for each request.
+    """
 
     def __init__(self, osc_in: OscIn, osc_out: OscOut):
         """
@@ -17,19 +19,23 @@ class OscQueryMaker:
         self.__osc_in = osc_in
         self.__osc_out = osc_out
         self.__query_register: dict[str, queue.SimpleQueue] = {}
-        osc_in.subscribe(self.__osc_query_maker_in)
+        osc_in.subscribe(self.__answer_listener)
 
-    def __osc_query_maker_in(self, msg: OscMsg) -> None:
+    def __answer_listener(self, msg: OscMsg) -> None:
         try:
             self.__query_register[msg.address].put(msg.data)
         except KeyError:
             pass
 
     def query(
-        self, address: str, timeout_sec: float = 2
+        self,
+        address: str,
+        data: str | bytes | bool | int | float | list | tuple = None,
+        *,
+        timeout_sec: float = 2,
     ) -> str | bytes | bool | float | list | tuple:
-        """Query data by sending the request to OSC address
-           and returning the response from that address
+        """Queries data by sending the request to OSC address
+           and returns the data of response from that address
 
         Args:
             address: OSC address to send request to
@@ -38,10 +44,16 @@ class OscQueryMaker:
         Returns:
             Response OSC message data
         """
+        if address in self.__query_register:
+            raise ValueError(
+                f'OSC query for {address} is already in progress. '
+                f'Parallel queries are not supported.'
+            )
+
         result_queue = queue.SimpleQueue()
         self.__query_register[address] = result_queue
         log("Requesting '{address}' data from {input}", address=address, input=self.__osc_in)
-        self.__osc_out.send(OscMsg(address))
+        self.__osc_out.send(OscMsg(address, data))
 
         try:
             result = result_queue.get(timeout=timeout_sec)
