58ddc7dca0725d486c0526e1bdc31ebdc1c00524
Maboroshy <maboroshy@mail.ru>
2024-07-27 16:25:21 +0300

    Implement mouse wheel int value changes for Message Sender widget

--- midiscripter/gui/message_sender.py
+++ midiscripter/gui/message_sender.py
@@ -1,10 +1,12 @@
 import inspect
+import re
 
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
 from midiscripter.base.port_base import Output
+from midiscripter.base.msg_base import Msg
 
 # Imports used by `eval`
 from midiscripter.midi import MidiMsg, ChannelMsg, SysexMsg, MidiType
@@ -21,6 +23,7 @@ class MessageSender(QWidget):
         super().__init__()
         self.setObjectName('Message Sender')
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
+        self.setMinimumWidth(220)
 
         layout = QGridLayout()
         self.setLayout(layout)
@@ -39,12 +42,8 @@ class MessageSender(QWidget):
         )
 
         self.output_selector = QComboBox()
-        self.output_selector.setPlaceholderText('Output Port')
         layout.addWidget(self.output_selector, 1, 0)
-        for port_name, port_instance in Output._instances.items():
-            self.output_selector.addItem(
-                f'{str(port_name)} ({port_instance.__class__.__name__})', port_instance
-            )
+        self.update_ports()
         self.output_selector.currentIndexChanged.connect(self.__set_output)
 
         self.send_button = QPushButton('Send')
@@ -62,21 +61,51 @@ class MessageSender(QWidget):
         if not self.msg:
             self.input_line.setText(current_text)
 
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        for match in re.finditer(r'-?\d+', self.input_line.text()):
+            start_pos, end_pos = match.span()
+            if start_pos <= self.input_line.cursorPosition() <= end_pos:
+                value = int(match.group(0))
+                value += 1 if event.angleDelta().y() > 0 else -1
+                text = self.input_line.text()
+                self.input_line.setText(text[:start_pos] + str(value) + text[end_pos:])
+                self.input_line.setCursorPosition(start_pos + len(str(value)))
+                return
+        else:
+            super().wheelEvent(event)
+
+    def update_ports(self) -> None:
+        self.output_selector.clear()
+
+        enabled_ports = [output for output in Output._instances if output.is_enabled]
+
+        if not enabled_ports:
+            self.output_selector.setPlaceholderText('No Enabled Output Ports')
+            self.output_selector.setDisabled(True)
+            return
+
+        self.output_selector.setPlaceholderText('Output Port')
+        self.output_selector.setDisabled(False)
+
+        for port in enabled_ports:
+            self.output_selector.addItem(f'{str(port._uid)} ({port.__class__.__name__})', port)
+        self.output_selector.currentIndexChanged.connect(self.__set_output)
+
     def __set_output(self, _: int) -> None:
         self.output = self.output_selector.currentData(Qt.ItemDataRole.UserRole)
         self.__validate()
 
     def __parse_input(self, input_text: str) -> None:
         try:
-            self.msg = eval(input_text)
+            object_for_input = eval(input_text)
+            self.msg = object_for_input if isinstance(object_for_input, Msg) else None
         except Exception:
             self.msg = None
         self.__validate()
 
     def __validate(self) -> None:
-        self.input_line.setStyleSheet(
-            f'background-color: {"white" if self.msg or not self.input_line.text() else "pink"}'
-        )
+        input_line_bg_color = 'white' if self.msg or not self.input_line.text() else 'pink'
+        self.input_line.setStyleSheet(f'background-color: {input_line_bg_color}')
 
         if not self.output or not self.msg:
             self.send_button.setDisabled(True)
