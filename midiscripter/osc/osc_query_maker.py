90ce2384fcbff77f035be6258501160c00f1e124
Maboroshy <maboroshy@mail.ru>
2025-01-07 23:38:43 +0300

    Reimplement OscQueryMaker for more stable behaviour

--- midiscripter/osc/osc_query_maker.py
+++ midiscripter/osc/osc_query_maker.py
@@ -1,4 +1,4 @@
-import queue
+import threading
 
 from midiscripter.logger import log
 from midiscripter.osc.osc_msg import OscMsg
@@ -18,47 +18,44 @@ class OscQueryMaker:
         """
         self.__osc_in = osc_in
         self.__osc_out = osc_out
-        self.__query_register: dict[str, queue.SimpleQueue] = {}
-        osc_in.subscribe(self.__answer_listener)
 
-    def __answer_listener(self, msg: OscMsg) -> None:
-        try:
-            self.__query_register[msg.address].put(msg.data)
-        except KeyError:
-            pass
+        self.__last_msg = OscMsg('')
+        self.__new_msg_condition = threading.Condition()
+
+        osc_in.subscribe(self.__osc_query_listener)
+
+    def __osc_query_listener(self, msg: OscMsg) -> None:
+        with self.__new_msg_condition:
+            self.__last_msg = msg
+            self.__new_msg_condition.notify_all()
 
     def query(
         self,
         address: str,
         data: str | bytes | bool | int | float | list | tuple = None,
         *,
-        timeout_sec: float = 2,
+        timeout_sec: float = 1,
     ) -> str | bytes | bool | float | list | tuple:
         """Queries data by sending the request to OSC address
            and returns the data of response from that address
 
         Args:
             address: OSC address to send request to
+            data: data to send request with, not used to match response
             timeout_sec: time for response until raising TimeoutError
 
         Returns:
             Response OSC message data
         """
-        if address in self.__query_register:
-            raise ValueError(
-                f'OSC query for {address} is already in progress. '
-                f'Parallel queries are not supported.'
+        with self.__new_msg_condition:
+            log(
+                "Requesting '{address}' data from OSC {input}", address=address, input=self.__osc_in
             )
+            self.__osc_out.send(OscMsg(address, data))
 
-        result_queue = queue.SimpleQueue()
-        self.__query_register[address] = result_queue
-        log("Requesting '{address}' data from {input}", address=address, input=self.__osc_in)
-        self.__osc_out.send(OscMsg(address, data))
-
-        try:
-            result = result_queue.get(timeout=timeout_sec)
-            self.__query_register.pop(address)
-            return result
-        except queue.Empty:
-            self.__query_register.pop(address)
-            raise TimeoutError from None
+            if self.__new_msg_condition.wait_for(
+                lambda: self.__last_msg.address == address, timeout=timeout_sec
+            ):
+                return self.__last_msg.data
+            else:
+                raise TimeoutError(f"OSC query to '{address}' got no response")
