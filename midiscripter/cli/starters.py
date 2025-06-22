a32ce973809d99e59783bd389c407d0cf1de26a5
Maboroshy <maboroshy@mail.ru>
2025-06-22 23:13:11 +0300

    Updated `start_cli_debug` to use new MIDI ports` methods

--- midiscripter/cli/starters.py
+++ midiscripter/cli/starters.py
@@ -19,10 +19,10 @@ def start_cli_debug() -> NoReturn:
 
     log('')
     log('Available MIDI inputs:')
-    [log.green(port_name) for port_name in midiscripter.midi.MidiIn._get_available_names]
+    [log.green(port_name) for port_name in midiscripter.midi.MidiIn._get_available_names()]
     log('')
     log('Available MIDI outputs:')
-    [log.magenta(port_name) for port_name in midiscripter.midi.MidiOut._available_names]
+    [log.magenta(port_name) for port_name in midiscripter.midi.MidiOut._get_available_names()]
     log('')
 
     _run_cli_loop()
