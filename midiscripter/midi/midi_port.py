422e94b2e1918591e8764e293d8aa015de9842f0
Maboroshy <maboroshy@mail.ru>
2026-09-19 11:30:40 +0300

    Fixed absent MIDI port handling

--- midiscripter/midi/midi_port.py
+++ midiscripter/midi/midi_port.py
@@ -111,8 +111,12 @@ class _MidiPortMixin(midiscripter.base.port_base.Port):
 
         except ValueError:
             log._port_not_found(self)
+            self._rtmidi_port.delete()
+            self._rtmidi_port = None
         except Exception:
             log._port_open(self, False)
+            self._rtmidi_port.delete()
+            self._rtmidi_port = None
 
     def _close(self) -> None:
         try:
